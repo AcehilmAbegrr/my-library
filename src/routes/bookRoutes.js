15619db7cfffa7e084606d471d66fb0e3034ba6e
Michael Garber <posterfearguy@gmail.com>
2019-09-16 06:37:01 -0400

    get single book view

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -30,7 +30,7 @@ const books = [{
 
 bookRouter.route('/')
   .get((req, res) => {
-    res.render('books', {
+    res.render('bookListView', {
       title: 'My Library',
       nav: [{
         link: '/books',
@@ -46,9 +46,23 @@ bookRouter.route('/')
   });
 
 
-bookRouter.route('/single')
+bookRouter.route('/:id')
+
   .get((req, res) => {
-    res.send('hello single book');
+    const { id } = req.params;
+    res.render('bookView', {
+      title: 'My Library',
+      nav: [{
+        link: '/books',
+        title: 'Books'
+      },
+      {
+        link: '/author',
+        title: 'Author'
+      }
+      ],
+      book: books[id]
+    });
   });
 
 module.exports = bookRouter;
