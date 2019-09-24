80c3f3c148003ca1b29ebcb9dcc29d5ce755b159
Michael Garber <posterfearguy@gmail.com>
2019-09-24 07:39:42 -0400

    get one book at a time

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,38 +1,12 @@
 const express = require('express');
 const {
-  MongoClient
+  MongoClient, ObjectID
 } = require('mongodb');
 const debug = require('debug')('app:bookRoutes');
 
 const bookRouter = express.Router();
 
 const router = (nav) => {
-  const books = [{
-    title: 'Of Mice and Men',
-    genre: 'Fiction',
-    author: 'John Steinbeck',
-    read: false
-  }, {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Leo Tolstoy',
-    read: false
-  }, {
-    title: 'Old Man and the Sea',
-    genre: 'Fiction',
-    author: 'Earnest Hemmingway',
-    read: false
-  }, {
-    title: 'For Whome the Bell Tolls',
-    genre: 'Fiction',
-    author: 'Earnest Hemmingway',
-    read: false
-  }, {
-    title: 'Atlas Shrugged',
-    genre: 'Fiction',
-    author: 'Ayn Rand',
-    read: false
-  }];
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
@@ -65,11 +39,28 @@ const router = (nav) => {
       const {
         id
       } = req.params;
-      res.render('bookView', {
-        title: 'My Library',
-        nav,
-        book: books[id]
-      });
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+      (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected to server');
+
+          const db = client.db(dbName);
+          const col = await db.collection('books');
+          const book = await col.findOne({ _id: new ObjectID(id) });
+          debug(book);
+          res.render('bookView', {
+            title: 'My Library',
+            nav,
+            book,
+          });
+          
+        } catch (error) {
+          debug(error.stack);
+        }
+      }());
     });
   return bookRouter;
 };
